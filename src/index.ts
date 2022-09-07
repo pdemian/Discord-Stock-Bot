4ee3cd05e18a6f518a2da11d5856c58706976495
Patrick Demian <patrick.demian@ryerson.ca>
2022-09-06 22:54:30 -0400

    Improved bot significantly and got non-deprecated package

--- src/index.ts
+++ src/index.ts
@@ -23,244 +23,287 @@ SOFTWARE.
 
 // Imports
 import Discord, { GatewayIntentBits } from 'discord.js';
-import yahooFinance from 'yahoo-finance';
+import yahooFinance from 'yahoo-finance2';
 import util from 'util';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
+import { Logger } from './logger';
+import { QuoteResponseArray, Quote } from 'yahoo-finance2/dist/esm/src/modules/quote';
+import getSymbolFromCurrency from 'currency-symbol-map'
 dotenv.config({ path: __dirname+'/../.env' });
 
 // Discord client token. Either replace with constant string or add to environment
 const token = process.env['DISCORD_TOKEN'];
+const log_path = process.env['LOG_FILE_PATH'] || '/tmp/discord-stock-bot/';
+const log_file = process.env['LOG_FILE'] || 'applog-{date}.log';
+const log_retention_days = Number.parseInt(process.env['LOG_RETENTION_DAYS'] || "7");
+
+const log = new Logger(log_path, log_file, log_retention_days);
 
 // Message regex
 const regex = /\$((?:[A-Za-z]{2,3}:[0-9]+)|(?:[A-Za-z0-9=\.\-\^]+)|(?:[0-9]+\.[A-Za-z]{2,3}))/g;
 
 // Specially handled tickers 
 const special_tickers = {
-	// Cryptocurrencies
-	'BTC'  : 'BTC-USD',
-	'ETH'  : 'ETH-USD',
-	'XRP'  : 'XRP-USD',
-	'LTC'  : 'LTC-USD',
-	'DOGE' : 'DOGE-USD',
-	'BNB'  : 'BNB-USD',
-	'ADA'  : 'ADA-USD',
-	'SOL'  : 'SOL-USD',
-	'XMR'  : 'XMR-USD',
-	
-	// Other currencies
-	'CAD'  : 'CADUSD=X',
-	'USD'  : 'CAD=X',
-	'EUR'  : 'EUR=X',
-	'GBP'  : 'GBP=X',
-	'JPY'  : 'JPY=X'
+  // Cryptocurrencies
+  'BTC'  : 'BTC-USD',
+  'ETH'  : 'ETH-USD',
+  'XRP'  : 'XRP-USD',
+  'LTC'  : 'LTC-USD',
+  'DOGE' : 'DOGE-USD',
+  'BNB'  : 'BNB-USD',
+  'ADA'  : 'ADA-USD',
+  'SOL'  : 'SOL-USD',
+  'XMR'  : 'XMR-USD',
+  
+  // Other currencies
+  'CAD'  : 'CADUSD=X',
+  'USD'  : 'CAD=X',
+  'EUR'  : 'EUR=X',
+  'GBP'  : 'GBP=X',
+  'JPY'  : 'JPY=X'
 };
 
 // Global supported market list
 const markets = {
-	// North America
-	"TSX" : ".TO", "TSE"  : ".TO",
-	"CVE" : ".V",
-	// Asia Pacific
-	"AU"  : ".AX", "ASX"  : ".AX",
-	"HK"  : ".HK", "HKG"  : ".HK",
-	"NZ"  : ".NZ", "NZE"  : ".NZE",
-	// Europe
-	"VI"  : ".VI", "WBAG" : ".VI",
-	"BT"  : ".BR", "EBR"  : ".BR",
-	"FR"  : ".PA", "EPA"  : ".PA",
-	"BE"  : ".BE", "BER"  : ".BE",
-	"ETR" : ".DE",
-	"FF"  : ".F",  "FRA"  : ".F",
-	"ST"  : ".SG", "STU"  : ".SG", 
-	"DB"  : ".IR", "ISE"  : ".IR",
-	"MI"  : ".MI", "BIT"  : ".MI",
-	"AE"  : ".AS", "AMS"  : ".AS",
-	"OS"  : ".OL", "OSL"  : ".OL",
-	"LB"  : ".LS", "ELI"  : ".LS",
-	"MD"  : ".MA", "MCE"  : ".MA",
-	"EB"  : ".VX", "VTX"  : ".VX",
-	"LN"  : ".L",  "LON"  : ".L",
-	// Middle East
-	"TV"  : ".TA", "TLV"  : ".TA"
-};
-
-interface QuoteData {
-	regularMarketChangePercent: number,
-	regularMarketChange: number,
-	regularMarketTime: Date,
-	regularMarketPrice: number,
-	regularMarketDayHigh: number,
-	regularMarketDayLow: number,
-	regularMarketVolume: number,
-	regularMarketPreviousClose: number,
-	regularMarketOpen: number,
-	symbol: string,
-	shortName: string,
-	longName: string | undefined,
-	currency: string,
-	currencySymbol: string,
-	marketCap: number | undefined
+  // North America
+  "TSX" : ".TO", "TSE"  : ".TO",
+  "CVE" : ".V",
+  // Asia Pacific
+  "AU"  : ".AX", "ASX"  : ".AX",
+  "HK"  : ".HK", "HKG"  : ".HK",
+  "NZ"  : ".NZ", "NZE"  : ".NZE",
+  // Europe
+  "VI"  : ".VI", "WBAG" : ".VI",
+  "BT"  : ".BR", "EBR"  : ".BR",
+  "FR"  : ".PA", "EPA"  : ".PA",
+  "BE"  : ".BE", "BER"  : ".BE",
+  "ETR" : ".DE",
+  "FF"  : ".F",  "FRA"  : ".F",
+  "ST"  : ".SG", "STU"  : ".SG", 
+  "DB"  : ".IR", "ISE"  : ".IR",
+  "MI"  : ".MI", "BIT"  : ".MI",
+  "AE"  : ".AS", "AMS"  : ".AS",
+  "OS"  : ".OL", "OSL"  : ".OL",
+  "LB"  : ".LS", "ELI"  : ".LS",
+  "MD"  : ".MA", "MCE"  : ".MA",
+  "EB"  : ".VX", "VTX"  : ".VX",
+  "LN"  : ".L",  "LON"  : ".L",
+  // Middle East
+  "TV"  : ".TA", "TLV"  : ".TA"
 };
 
 class QuoteMessage {
-	private readonly red   = 0xFF0000;
-	private readonly green = 0x00FF00;
+  private readonly red   = 0xFF0000;
+  private readonly green = 0x00FF00;
 
-	public readonly sign: boolean;
-	public readonly sign_symbol: string;
-	public readonly currency_symbol: string;
-	public readonly name: string;
-	public readonly symbol: string;
-	public readonly date: Date;
-	public readonly price: string;
-	public readonly change: string;
-	public readonly change_percent: string;
-	public readonly high: string;
-	public readonly low: string;
-	public readonly prev: string;
-	public readonly volume: string;
-	public readonly market_cap: string;
+  public readonly sign: boolean;
+  public readonly sign_symbol: string;
+  public readonly currency_symbol: string;
+  public readonly name: string;
+  public readonly symbol: string;
+  public readonly date: Date;
+  public readonly price: string;
+  public readonly change: string;
+  public readonly change_percent: string;
+  public readonly high: string;
+  public readonly low: string;
+  public readonly prev: string;
+  public readonly volume: string;
+  public readonly market_cap: string;
+  public readonly pe: string;
+  public readonly eps: string;
+  public readonly div_yield: string;
+  public readonly market_state: "REGULAR" | "CLOSED" | "PRE" | "PREPRE" | "POST" | "POSTPOST";
+  public readonly prepost_price: string = "";
+  public readonly prepost_change: string = "";
+  public readonly prepost_change_percent: string = "";
+  public readonly prepost_sign: string = "";
+  private readonly market_status?: string;
 
-	constructor(data: QuoteData) {
-		// Extract and format the data
-		this.sign           = data.regularMarketChange >= 0;
-		this.sign_symbol    = this.sign ? '+' : '';
-		this.symbol         = data.symbol;
-		this.date           = data.regularMarketTime;
-		this.price          = this.round(data.regularMarketPrice);
-		this.change         = this.round(data.regularMarketChange);
-		this.change_percent = this.round(data.regularMarketChangePercent * 100);
-		this.high           = this.round(data.regularMarketDayHigh);
-		this.low            = this.round(data.regularMarketDayLow);
-		this.prev           = this.round(data.regularMarketPreviousClose);
+  constructor(data: Quote) {
+    // Extract and format the data
+    this.sign           = data.regularMarketChange! >= 0;
+    this.sign_symbol    = this.sign ? '+' : '';
+    this.symbol         = data.symbol;
+    this.date           = data.regularMarketTime!;
+    this.price          = this.round(data.regularMarketPrice);
+    this.change         = this.round(data.regularMarketChange);
+    this.change_percent = this.round(data.regularMarketChangePercent);
+    this.high           = this.round(data.regularMarketDayHigh);
+    this.low            = this.round(data.regularMarketDayLow);
+    this.prev           = this.round(data.regularMarketPreviousClose);
+    this.market_state   = data.marketState;
+    this.pe             = this.round(data.forwardPE);
+    this.eps            = this.round(data.epsForward);
+    this.div_yield      = data.trailingAnnualDividendYield ? this.round(data.trailingAnnualDividendYield * 100) : 'N/A';
 
-		// Currencies don't have volumes or market caps
-		this.market_cap = data.marketCap == undefined ? 'N/A' : this.shortForm(data.marketCap);
-		this.volume = data.regularMarketVolume == undefined || data.regularMarketVolume < 1 ? 'N/A' : this.shortForm(data.regularMarketVolume);
+    // Currencies don't have volumes or market caps
+    this.market_cap = data.marketCap == undefined ? 'N/A' : this.shortForm(data.marketCap);
+    this.volume = data.regularMarketVolume == undefined || data.regularMarketVolume < 1 ? 'N/A' : this.shortForm(data.regularMarketVolume);
 
-		// Some currencies don't have a specific symbol and will repeat the currency name
-		this.currency_symbol = data.currency + (data.currency === data.currencySymbol ? '' : data.currencySymbol);
-		
-		// Currencies don't have long names, but long names are prefered for stocks
-		this.name = data.longName == null ? data.shortName : data.longName;
-	}
+    this.currency_symbol = data.currency ? data.currency +  getSymbolFromCurrency(data.currency) : "";
+    
+    // Currencies don't have long names, but long names are preferred for stocks
+    this.name = data.longName === undefined || data.longName === null ? data.shortName! : data.longName;
 
-	// Rounds digits to 2 decimal places, using scientific notation if the number is too small 
-	private round(num: number): string {
-		if(Math.abs(num) < 0.01) {
-			return num.toExponential(2);
-		}
-		return num.toFixed(2);
-	}
-	
-	// Creates a short hand number
-	private shortForm(num: number): string {
-		const log = Math.log10(num);
-	
-		if(log > 12) { return this.round(num / 1e12) + 'T'; }
-		else if(log > 9) { return this.round(num / 1e9) + 'B'; }
-		else if(log > 6) { return this.round(num / 1e6) + 'M'; }
-		else if(log > 3) { return this.round(num / 1e3) + 'K'; } 
-		return this.round(num);
-	}
+    if(data.preMarketPrice || data.postMarketPrice) {
+      switch(this.market_state) {
+        case "REGULAR":
+          this.market_status = undefined;
+          break;
+        case "PRE":
+          this.market_status = "PreMkt";
+          this.prepost_price = this.round(data.preMarketPrice);
+          this.prepost_change = this.round(data.preMarketChange);
+          this.prepost_change_percent = this.round(data.preMarketChangePercent);
+          this.prepost_sign = data.preMarketChange! >= 0 ? '+' : '';
+          break;
+        default:
+          this.market_status = "PostMkt";
+          this.prepost_price = this.round(data.postMarketPrice);
+          this.prepost_change = this.round(data.postMarketChange);
+          this.prepost_change_percent = this.round(data.postMarketChangePercent);
+          this.prepost_sign = data.postMarketChange! >= 0 ? '+' : '';
+          break;
+      }
+    }
+    else {
+      this.market_status = undefined;
+    }
+  }
 
-	public toEmbed() {
-		const description = 
-			util.format('%s%s %s%s (%s%s%%)\n', this.currency_symbol, this.price, this.sign_symbol, this.change, this.sign_symbol, this.change_percent) + 
-			util.format('High: %s, Low: %s, Prev: %s\n', this.high, this.low, this.prev) +
-			util.format('Cap: %s, Volume: %s', this.market_cap, this.volume);
+  // Rounds digits to 2 decimal places, using scientific notation if the number is too small 
+  private round(num: number | undefined): string {
+    if(num === undefined) {
+      return 'N/A';
+    }
+    if(Math.abs(num) < 0.01) {
+      return num.toExponential(2);
+    }
+    return num.toFixed(2);
+  }
+  
+  // Creates a short hand number
+  private shortForm(num: number): string {
+    const log = Math.log10(num);
+  
+    if(log > 12) { return this.round(num / 1e12) + 'T'; }
+    else if(log > 9) { return this.round(num / 1e9) + 'B'; }
+    else if(log > 6) { return this.round(num / 1e6) + 'M'; }
+    else if(log > 3) { return this.round(num / 1e3) + 'K'; } 
+    return this.round(num);
+  }
 
-		return { embeds: [new Discord.EmbedBuilder()
-			.setColor(this.sign ? this.green : this.red)
-			.setAuthor({ name: util.format('%s (%s)', this.name, this.symbol) })
-			.setTimestamp(this.date)
-			.setFooter({ text: 'Via Yahoo! Finance. Delayed 15 min' })
-			.setDescription(description) ]};
-	}
+  public toEmbed() {
+    const prepost_market = 
+      this.market_status ? util.format('%s: %s %s%s (%s%s%%)\n', this.market_status, this.prepost_price, this.prepost_sign, this.prepost_change, this.prepost_sign, this.prepost_change_percent) : "";
+
+    const description = 
+      util.format('%s%s %s%s (%s%s%%)\n', this.currency_symbol, this.price, this.sign_symbol, this.change, this.sign_symbol, this.change_percent) + 
+      prepost_market +
+      util.format('High: %s, Low: %s, Prev: %s\n', this.high, this.low, this.prev) +
+      util.format('P/E Ratio: %s, EPS: %s, Div Yield: %s%%\n', this.pe, this.eps, this.div_yield) +
+      util.format('Cap: %s, Volume: %s', this.market_cap, this.volume);
+
+    return { embeds: [new Discord.EmbedBuilder()
+      .setColor(this.sign ? this.green : this.red)
+      .setAuthor({ name: util.format('%s (%s)', this.name, this.symbol) })
+      .setTimestamp(this.date)
+      .setFooter({ text: 'Via Yahoo! Finance. Delayed 15 min' })
+      .setDescription(description) ]};
+  }
 }
 
 function unhandledException(err: Error): void {
-	console.error('Uncaught Exception: ');
-	console.error(err);
+  log.error('Uncaught Exception: ');
+  log.exception(err);
 }
 
 function onReady(): void {
-	console.log('I am ready!');
+  log.info('I am ready!');
 }
 
 // Loop through all parsed tickers and extract them. Empty array if none
 function parseTickers(message: string): string[] {
-	const symbols: string[] = [];
+  const symbols: string[] = [];
 
-	for(let match = regex.exec(message); match !== null; match = regex.exec(message)) {
-		const ticker = match[1].toUpperCase();
-		
-		if(/^[0-9]/.test(ticker)) {
-			if(!/\.[A-Za-z]{2,}/.test(ticker)) {
-				continue;
-			}
-		}
-		else if(!/[A-Za-z]/.test(ticker)) {
-			continue;
-		}
+  for(let match = regex.exec(message); match !== null; match = regex.exec(message)) {
+    const ticker = match[1].toUpperCase();
+    
+    if(/^[0-9]/.test(ticker)) {
+      if(!/\.[A-Za-z]{2,}/.test(ticker)) {
+        continue;
+      }
+    }
+    else if(!/[A-Za-z]/.test(ticker)) {
+      continue;
+    }
+    
+    log.info(`Found request for ticker: ${ticker}`);
 
-		if(special_tickers[ticker] !== undefined) {
-			symbols.push(special_tickers[ticker]);
-		}
-		else {
-			const index = ticker.indexOf(':');
-			// If we have MARKET:TICKER, replace with TICKER.MKT
-			if(index > 0 && ticker.length > index) {
-				const market = ticker.substring(0, index);
-				const mkt = markets[market] === undefined ? '' : markets[market];
+    if(special_tickers[ticker] !== undefined) {
+      symbols.push(special_tickers[ticker]);
+    }
+    else {
+      const index = ticker.indexOf(':');
+      // If we have MARKET:TICKER, replace with TICKER.MKT
+      if(index > 0 && ticker.length > index) {
+        const market = ticker.substring(0, index);
+        const mkt = markets[market] === undefined ? '' : markets[market];
+        const newSymbol = ticker.substring(index+1, ticker.length) + mkt;
+        symbols.push(newSymbol);
+        log.info(`Modified to become ${newSymbol}`);
+      }
+      else {
+        symbols.push(ticker);
+      }
+    }
+  }
 
-				symbols.push(ticker.substring(index+1, ticker.length) + mkt);
-			}
-			else {
-				symbols.push(ticker);
-			}
-		}
-	}
-
-	return symbols;
+  return symbols;
 }
 
 function onMessage(message: Discord.Message): void {
-	const symbols = parseTickers(message.content);
-	if(symbols.length == 0) return;
+  const symbols = parseTickers(message.content);
+  if(symbols.length == 0) return;
+  log.info("Expected symbols: " + JSON.stringify(symbols));
 
-	yahooFinance.quote({
-		symbols: symbols,
-		modules: [ 'price' ]
-	}, (err: Error, quotes: any) => {
-		if(err) {
-			message.channel.send('Stock(s) not found or API error');
-			console.error(err.message);
-		}
-		else {
-			for(const symbol of symbols) {
-				const data = quotes[symbol]['price'] as QuoteData;
+  yahooFinance.quote(symbols).then(async (quotes: QuoteResponseArray) => {
+    log.info("Got symbol info from Yahoo");
+    for(const data of quotes) {
+      const symbol = data.symbol;
 
-				if(data.regularMarketPrice === undefined) {
-					message.channel.send('Price for ' + symbol + ' could not be found');
-					continue;
-				}
+      if(data.regularMarketPrice === undefined) {
+        message.channel.send(`Price for ${symbol} could not be found`);
+        log.info(`Price for ${symbol} could not be found`);
+        continue;
+      }
 
-				message.channel.send(new QuoteMessage(data).toEmbed());
-			}
-		}
-	});
+      try {
+        await message.channel.send(new QuoteMessage(data).toEmbed());
+      }
+      catch(error) {
+        message.channel.send(`There was an issue retrieving ${symbol}`);
+        log.error(`There was an issue retrieving ${symbol}`);
+        log.ex(error);
+      }
+    }
+  }).catch((err) => {
+    message.channel.send('Stock(s) not found or API error');
+    log.error("Failed to query stocks");
+    log.ex(err);
+  });
 }
 
 function main(): void {
-	// Handle unhandled exceptions
-	process.on('uncaughtException', unhandledException);
+  // Handle unhandled exceptions
+  process.on('uncaughtException', unhandledException);
 
-	// Initialize discord 
-	const client = new Discord.Client({intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ]});
-	client.on('ready', onReady);
-	client.on('messageCreate', onMessage);
-	client.login(token);
+  // Initialize discord 
+  const client = new Discord.Client({intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ]});
+  client.on('ready', onReady);
+  client.on('messageCreate', onMessage);
+  client.login(token);
 }
 
 main();
---
+++ src/logger.ts
@@ -0,0 +1,72 @@
+/*
+Copyright (c) 2022 Patrick Demian
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class Logger {
+  constructor(private log_path: string, private log_file: string, private retention_days: number) {  
+    setInterval(this.cleanOldLogFiles, 24 * 60 * 60 * 1000);
+  }
+
+  private log(type: string, message: string) {
+    const todays_date = new Date();
+    const todays_file = path.join(this.log_path, this.log_file.replace("{date}", `${todays_date.getUTCFullYear()}-${todays_date.getUTCMonth()}-${todays_date.getUTCDay()}`));
+
+    fs.appendFileSync(todays_file, `[${todays_date.toISOString()}] ${type}: ${message}\n`);
+  }
+
+  private cleanOldLogFiles() {
+    const todays_date = new Date();
+    for(const filename of fs.readdirSync(this.log_path)) {
+      let match = /(\d{4}-\d{1,2}-\d{1,2})/.exec(filename);
+      if(match) {
+        const file_date = Date.parse(match[1]);
+        const days_diff = Math.abs(file_date - todays_date.getTime()) / (1000 * 60 * 60 * 24);
+        if(days_diff > this.retention_days) {
+          fs.unlinkSync((filename));
+        }
+      }
+    }
+  }
+
+  public info(message: string) {
+    this.log('INFO', message);
+  }
+
+  public warn(message: string) {
+    this.log('WARN', message);
+  }
+
+  public error(message: string) {
+    this.log('ERROR', message);
+  }
+
+  public exception(error: Error) {
+    this.log('EXCEPTION', `${error.name} ${error.message}:\n ${error.stack}`);
+  }
+
+  public ex(error: any) {
+    this.log('EXCEPTION', `${JSON.stringify(error)}`);
+  }
+}
